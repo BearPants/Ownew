82b6087792ab049236dbe8a43eda16c8fabe8409
Jaegwan <kjg5417@gmail.com>
2015-11-23 23:25:20 +0900

    2015_11_23_김재관
    진짜최종

--- app/src/main/java/com/example/kang_won/widgetex/tempActivity.java
+++ app/src/main/java/com/example/kang_won/widgetex/tempActivity.java
@@ -3,13 +3,14 @@ package com.example.kang_won.widgetex;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 
 public class tempActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         Intent receivedIntent = getIntent();
         int widgetID = receivedIntent.getIntExtra("WidgetID", 0);
         Intent intent = new Intent(tempActivity.this, WidgetReceiver.class);
